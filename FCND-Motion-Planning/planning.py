290568d0523595fad2a765085a8341245a5baab0
ay1011 <yongallan@yahoo.com>
2021-05-16 18:56:39 +0800

    Add Medial Axis Solution
    Also updated planning.py to allow for diagonal actions

--- FCND-Motion-Planning/planning.py
+++ FCND-Motion-Planning/planning.py
@@ -1,7 +1,9 @@
 from enum import Enum
 from queue import PriorityQueue
+import numpy as np
 
 
+# Quadroter assume all actions cost the same.
 class Action(Enum):
     """
     An action is represented by a 3 element tuple.
@@ -10,20 +12,15 @@ class Action(Enum):
     to the current grid position. The third and final value
     is the cost of performing the action.
     """
-    LEFT = (0, -1, 1)
-    RIGHT = (0, 1, 1)
-    UP = (-1, 0, 1)
-    DOWN = (1, 0, 1)
 
-    def __str__(self):
-        if self == self.LEFT:
-            return '<'
-        elif self == self.RIGHT:
-            return '>'
-        elif self == self.UP:
-            return '^'
-        elif self == self.DOWN:
-            return 'v'
+    WEST = (0, -1, 1)
+    EAST = (0, 1, 1)
+    NORTH = (-1, 0, 1)
+    SOUTH = (1, 0, 1)
+    NORTH_WEST = (-1, -1, np.sqrt(2))
+    NORTH_EAST = (-1, 1, np.sqrt(2))
+    SOUTH_WEST = (1, -1, np.sqrt(2))
+    SOUTH_EAST = (1, 1, np.sqrt(2))
 
     @property
     def cost(self):
@@ -38,7 +35,7 @@ def valid_actions(grid, current_node):
     """
     Returns a list of valid actions given a grid and current node.
     """
-    valid = [Action.UP, Action.LEFT, Action.RIGHT, Action.DOWN]
+    valid_actions = list(Action)
     n, m = grid.shape[0] - 1, grid.shape[1] - 1
     x, y = current_node
 
@@ -46,19 +43,27 @@ def valid_actions(grid, current_node):
     # it's an obstacle
 
     if x - 1 < 0 or grid[x - 1, y] == 1:
-        valid.remove(Action.UP)
+        valid_actions.remove(Action.NORTH)
     if x + 1 > n or grid[x + 1, y] == 1:
-        valid.remove(Action.DOWN)
+        valid_actions.remove(Action.SOUTH)
     if y - 1 < 0 or grid[x, y - 1] == 1:
-        valid.remove(Action.LEFT)
+        valid_actions.remove(Action.WEST)
     if y + 1 > m or grid[x, y + 1] == 1:
-        valid.remove(Action.RIGHT)
+        valid_actions.remove(Action.EAST)
 
-    return valid
+    if (x - 1 < 0 or y - 1 < 0) or grid[x - 1, y - 1] == 1:
+        valid_actions.remove(Action.NORTH_WEST)
+    if (x - 1 < 0 or y + 1 > m) or grid[x - 1, y + 1] == 1:
+        valid_actions.remove(Action.NORTH_EAST)
+    if (x + 1 > n or y - 1 < 0) or grid[x + 1, y - 1] == 1:
+        valid_actions.remove(Action.SOUTH_WEST)
+    if (x + 1 > n or y + 1 > m) or grid[x + 1, y + 1] == 1:
+        valid_actions.remove(Action.SOUTH_EAST)
+
+    return valid_actions
 
 
 def a_star(grid, h, start, goal):
-
     path = []
     path_cost = 0
     queue = PriorityQueue()
@@ -67,16 +72,16 @@ def a_star(grid, h, start, goal):
 
     branch = {}
     found = False
-    
+
     while not queue.empty():
         item = queue.get()
         current_node = item[1]
         if current_node == start:
             current_cost = 0.0
-        else:              
+        else:
             current_cost = branch[current_node][0]
-            
-        if current_node == goal:        
+
+        if current_node == goal:
             print('Found a path.')
             found = True
             break
@@ -87,12 +92,12 @@ def a_star(grid, h, start, goal):
                 next_node = (current_node[0] + da[0], current_node[1] + da[1])
                 branch_cost = current_cost + action.cost
                 queue_cost = branch_cost + h(next_node, goal)
-                
-                if next_node not in visited:                
-                    visited.add(next_node)               
+
+                if next_node not in visited:
+                    visited.add(next_node)
                     branch[next_node] = (branch_cost, current_node, action)
                     queue.put((queue_cost, next_node))
-             
+
     if found:
         # retrace steps
         n = goal
@@ -105,6 +110,7 @@ def a_star(grid, h, start, goal):
     else:
         print('**********************')
         print('Failed to find a path!')
-        print('**********************') 
+        print('**********************')
     return path[::-1], path_cost
+
 
